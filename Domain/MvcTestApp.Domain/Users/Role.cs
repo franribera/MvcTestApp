2c8aa5dacf1f7d2dbec7f4440e58cf3f2a9a32b8
Francesc Ribera Hidalgo <fribera@plainconcepts.com>
2019-03-02 16:30:35 +0100

    Added Role entity. Create Agrregate User with user as aggregate root.

---
+++ Domain/MvcTestApp.Domain/Users/Role.cs
@@ -0,0 +1,19 @@
+﻿using MvcTestApp.Domain.Infrastructure;
+using MvcTestApp.Domain.ValueObjects;
+
+namespace MvcTestApp.Domain.Users
+{
+    public class Role : Entity
+    {
+        public static Role PAGE_1 = new Role(new Name("PAGE_1"));
+        public static Role PAGE_2 = new Role(new Name("PAGE_2"));
+        public static Role PAGE_3 = new Role(new Name("PAGE_3"));
+
+        public Name Name { get; protected set; }
+
+        public Role(Name name)
+        {
+            Name = name;
+        }
+    }
+}
--- Domain/MvcTestApp.Domain/Users/User.cs
+++ Domain/MvcTestApp.Domain/Users/User.cs
@@ -1,13 +1,26 @@
-﻿using MvcTestApp.Domain.ValueObjects;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using MvcTestApp.Domain.Infrastructure;
+using MvcTestApp.Domain.ValueObjects;
 
 namespace MvcTestApp.Domain.Users
 {
-    public class User
+    public class User : Aggregate
     {
+        private ICollection<Role> _roles;
+
         public Name UserName { get; protected set; }
         public Password Password { get; protected set; }
+        public ReadOnlyCollection<Role> Roles => _roles.ToList().AsReadOnly();
 
-        public User(Name userName, Password password)
+        protected User()
+        {
+            _roles = new List<Role>();
+        }
+
+        public User(Name userName, Password password) : this()
         {
             UserName = userName;
             Password = password;
