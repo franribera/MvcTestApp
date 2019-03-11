c8155c4d84cae81f095f84ac4ccb0fca1a5c3a19
Francesc Ribera Hidalgo <fribera@plainconcepts.com>
2019-03-11 01:37:58 +0100

    AuthenticationService code style

--- MvcTestApp/Authentication/AuthenticationService.cs
+++ MvcTestApp/Authentication/AuthenticationService.cs
@@ -1,7 +1,6 @@
-﻿    using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using MvcTestApp.Domain.Users;
