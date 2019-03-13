f21114dcb585db6597e4020fbfc9f8adf73d8d09
Francesc Ribera Hidalgo <fribera@plainconcepts.com>
2019-03-13 11:01:16 +0100

    Added cookies and redirections to testServer default implementation. Added more functional tests.

--- MvcTestApp.FunctionalTests/FunctionalTests.cs
+++ MvcTestApp.FunctionalTests/FunctionalTests.cs
@@ -1,9 +1,7 @@
+using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,7 +10,7 @@ namespace MvcTestApp.FunctionalTests
     [TestClass]
     public class FunctionalTests
     {
-        private HttpClient _testClient;
+        private Server _server;
 
         [TestInitialize]
         public void TestInitialize()
@@ -21,7 +19,7 @@ namespace MvcTestApp.FunctionalTests
                 .UseEnvironment("Development")
                 .UseStartup<Startup>();
             var testServer = new TestServer(builder);
-            _testClient = testServer.CreateClient();
+            _server = new Server(testServer);
         }
 
         [TestMethod]
@@ -29,14 +27,13 @@ namespace MvcTestApp.FunctionalTests
         {
             // Arrange
             const string destinationPage = "/Page1";
-            const HttpStatusCode expectedHttpCode = HttpStatusCode.Redirect;
             const string expectedRedirectedPage = "/login/login";
 
             // Act
-            var response = await _testClient.GetAsync(destinationPage);
+            var response = await _server.Get(destinationPage);
 
             // Assert
-            Assert.AreEqual(expectedHttpCode, response.StatusCode);
+            Assert.AreEqual(HttpStatusCode.Redirect, response.StatusCode);
             Assert.AreEqual(destinationPage, response.RequestMessage.RequestUri.AbsolutePath);
             Assert.AreEqual(expectedRedirectedPage, response.Headers.Location.AbsolutePath);
         }
@@ -45,41 +42,90 @@ namespace MvcTestApp.FunctionalTests
         public async Task User_Login_Failed_No_Redirection_Is_Performed()
         {
             // Arrange
-            const string userName = "NotExistingUser";
-            const string password = "NotExistingPassword";
-            const HttpStatusCode expectedHttpCode = HttpStatusCode.OK;
-            const string destinationPage = "/Page1";
-
-            var content = new StringContent($"username={userName}&password={password}&returnurl={destinationPage}",
-                Encoding.UTF8,
-                "application/x-www-form-urlencoded");
+            var postValues = new Dictionary<string, string>
+            {
+                {"username", "NotExistingUser"},
+                {"password", "NotExistingPassword"},
+                {"returnurl", "/Page1"}
+            };
 
             // Act
-            var response = await _testClient.PostAsync("/login/login", content);
+            var response = await _server.Post("/login/login", postValues);
 
             // Assert
-            Assert.AreEqual(expectedHttpCode, response.StatusCode);
-            Assert.IsNull(response.Headers.Location);
+            Assert.AreNotEqual(HttpStatusCode.Redirect, response.StatusCode);
         }
 
         [TestMethod]
         public async Task User_Logs_In_And_Is_Redirected_To_Original_Requested_Page()
         {
             // Arrange
-            const string userName = "User1";
-            const string password = "User1";
-            const HttpStatusCode expectedHttpCode = HttpStatusCode.Redirect;
-            const string originalRequestedPage = "Page1";
-            var content = new StringContent($"username={userName}&password={password}&returnurl={originalRequestedPage}",
-                Encoding.UTF8,
-                "application/x-www-form-urlencoded");
+            const string destinationPage = "/Page1";
+            var postValues = new Dictionary<string, string>
+            {
+                {"username", "User1"},
+                {"password", "User1"},
+                {"returnurl", destinationPage}
+            };
 
             // Act
-            var response = await _testClient.PostAsync("/login/login", content);
+            var loginResponse = await _server.Post("/login/login", postValues);
+            var response = await _server.FollowRedirect(loginResponse);
 
             // Assert
-            Assert.AreEqual(expectedHttpCode, response.StatusCode);
-            Assert.AreEqual(originalRequestedPage, response.Headers.Location.ToString());
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual(destinationPage, response.RequestMessage.RequestUri.AbsolutePath);
+        }
+
+        [DataTestMethod]
+        [DataRow("User1", true)]
+        [DataRow("User2", false)]
+        [DataRow("User3", false)]
+        [DataRow("Admin", true)]
+        public async Task Page1_Can_Only_Be_Accessed_By_PAGE1_Role(string userName, bool expectedRedirectionPage)
+        {
+            await TestPageAccess("/Page1", userName, expectedRedirectionPage);
+        }
+
+        [DataTestMethod]
+        [DataRow("User1", false)]
+        [DataRow("User2", true)]
+        [DataRow("User3", false)]
+        [DataRow("Admin", true)]
+        public async Task Page2_Can_Only_Be_Accessed_By_PAGE2_Role(string userName, bool expectedRedirectionPage)
+        {
+            await TestPageAccess("/Page2", userName, expectedRedirectionPage);
+        }
+
+        [DataTestMethod]
+        [DataRow("User1", false)]
+        [DataRow("User2", false)]
+        [DataRow("User3", true)]
+        [DataRow("Admin", true)]
+        public async Task Page3_Can_Only_Be_Accessed_By_PAGE3_Role(string userName, bool expectedRedirectionPage)
+        {
+            await TestPageAccess("/Page3", userName, expectedRedirectionPage);
+        }
+
+        private async Task TestPageAccess(string intendedDestinationPage, string userName, bool accessGranted)
+        {
+            // Arrange
+            const string unauthorieze = "/login/Unauthorize";
+
+            var postValues = new Dictionary<string, string>
+            {
+                {"username", userName},
+                {"password", userName},
+                {"returnurl", intendedDestinationPage}
+            };
+
+            // Act
+            var loginResponse = await _server.Post("/login/login", postValues);
+            var redirectResponse = await _server.FollowRedirect(loginResponse);
+            var response = await _server.FollowRedirect(redirectResponse);
+
+            // Assert
+            Assert.AreEqual(accessGranted, response.RequestMessage.RequestUri.AbsolutePath != unauthorieze);
         }
     }
 }
---
+++ MvcTestApp.FunctionalTests/Server.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Net.Http.Headers;
+
+namespace MvcTestApp.FunctionalTests
+{
+    //https://reasoncodeexample.com/2016/08/29/how-to-keep-things-tidy-when-using-asp-net-core-testserver/
+    public class Server
+    {
+        private readonly TestServer _testServer;
+
+        public Server(TestServer testServer)
+        {
+            _testServer = testServer;
+            Cookies = new CookieContainer();
+        }
+
+        public CookieContainer Cookies { get; }
+
+        public async Task<HttpResponseMessage> Get(string relativeUrl)
+        {
+            return await Get(new Uri(relativeUrl, UriKind.Relative));
+        }
+
+        public async Task<HttpResponseMessage> Get(Uri relativeUrl)
+        {
+            var absoluteUrl = new Uri(_testServer.BaseAddress, relativeUrl);
+            var requestBuilder = _testServer.CreateRequest(absoluteUrl.ToString());
+            AddCookies(requestBuilder, absoluteUrl);
+            var response = await requestBuilder.GetAsync();
+            UpdateCookies(response, absoluteUrl);
+            return response;
+        }
+
+        private void AddCookies(RequestBuilder requestBuilder, Uri absoluteUrl)
+        {
+            var cookieHeader = Cookies.GetCookieHeader(absoluteUrl);
+            if (!string.IsNullOrWhiteSpace(cookieHeader))
+            {
+                requestBuilder.AddHeader(HeaderNames.Cookie, cookieHeader);
+            }
+        }
+
+        private void UpdateCookies(HttpResponseMessage response, Uri absoluteUrl)
+        {
+            if (response.Headers.Contains(HeaderNames.SetCookie))
+            {
+                var cookies = response.Headers.GetValues(HeaderNames.SetCookie);
+                foreach (var cookie in cookies)
+                {
+                    Cookies.SetCookies(absoluteUrl, cookie);
+                }
+            }
+        }
+
+        public async Task<HttpResponseMessage> Post(string relativeUrl, IDictionary<string, string> formValues)
+        {
+            return await Post(new Uri(relativeUrl, UriKind.Relative), formValues);
+        }
+
+        public async Task<HttpResponseMessage> Post(Uri relativeUrl, IDictionary<string, string> formValues)
+        {
+            var absoluteUrl = new Uri(_testServer.BaseAddress, relativeUrl);
+            var requestBuilder = _testServer.CreateRequest(absoluteUrl.ToString());
+            AddCookies(requestBuilder, absoluteUrl);
+            var content = new FormUrlEncodedContent(formValues);
+            var response = await requestBuilder.And(message =>
+            {
+                message.Content = content;
+            }).PostAsync();
+            UpdateCookies(response, absoluteUrl);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> FollowRedirect(HttpResponseMessage response)
+        {
+            if (response.StatusCode != HttpStatusCode.Moved && response.StatusCode != HttpStatusCode.Found)
+            {
+                return response;
+            }
+            var redirectUrl = new Uri(response.Headers.Location.ToString(), UriKind.RelativeOrAbsolute);
+            if (redirectUrl.IsAbsoluteUri)
+            {
+                redirectUrl = new Uri(redirectUrl.PathAndQuery, UriKind.Relative);
+            }
+            return await Get(redirectUrl);
+        }
+    }
+}
